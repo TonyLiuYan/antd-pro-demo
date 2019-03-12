0a8e8bb3a91c0954224dcfdebeead121b250262b
liuyan <liuyan22@vip.taxchina.com>
2019-03-12 13:35:56 +0800

    1.0

---
+++ src/pages/HelloWorld/HelloWorld.js
@@ -0,0 +1,13 @@
+import React, { PureComponent } from 'react';
+
+class HelloWorld extends PureComponent {
+    render() {
+        return (
+            <div>
+                Hello World
+            </div>
+        );
+    }
+}
+
+export default HelloWorld;
